db72d38692111ccfcd53f6379d7b961e80da163b
giovanni.ciccarello <giovannigc91@gmail.com>
2021-11-28 00:05:42 +0100

    add private classes Calculator and OperatorNotSupportedException

--- it/ggc/calculator/App.java
+++ it/ggc/calculator/App.java
@@ -1,33 +1,51 @@
 package it.ggc.calculator;
 
 public class App {
+	
+	private static class Calculator {
+		
+		public int executeOperation(final String operator, final int operand1, final int operand2)
+		   throws OperatorNotSupportedException {
+		   int result = 0;
+			if("sum".equals(operator)) {
+				result = operand1 + operand2;
+			} else {
+			   throw new OperatorNotSupportedException();
+			}
+			return result;
+		}
+		
+	}
+	
+	private static class OperatorNotSupportedException extends Exception {
+		
+		public OperatorNotSupportedException() {
+			super();
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		
-		if(args.length < 2) {
-			System.out.println("Please, provide an operator and some arguments, like this: ");
+		if(args.length < 3) {
+			System.out.println("Please, provide an operator and two operands, like this: ");
 			System.out.println("sum 1 2");
 			return;
 		}
 		
 		String operator = args[0];
-		int result = 0;
-		if("sum".equals(operator)) {
-			if(args.length < 3) {
-			   System.out.println("Please, provide at least two operators like this: ");
-			   System.out.println("sum 1 2");
-			   return;
-			}
-			int operand1 = Integer.parseInt(args[1]);
-			int operand2 = Integer.parseInt(args[2]);
-			result = operand1 + operand2;
-		} else {
-		   System.out.println("Operator not supported. List of supported operators: ");
-		   System.out.println("sum");
-		   return;
+		int operand1 = Integer.parseInt(args[1]);
+		int operand2 = Integer.parseInt(args[2]);
+		
+		Calculator calculator = new Calculator();
+		try {
+			int result = calculator.executeOperation(operator, operand1, operand2);
+			System.out.println("Result is " + result);
+		} catch(OperatorNotSupportedException onse) {
+			System.out.println("Operator not supported. List of supported operators: ");
+			System.out.println("sum");
 		}
 		
-		System.out.println("Result is " + result);
 	}
 
 }
